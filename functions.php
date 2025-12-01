939994b256d207f3092db00566c16fe8639da3c4
Liukin <contacto@pc-solucion.es>
2025-12-01 14:18:10 +0100

    Remove README.md and add WordPress security hardening functions.

--- functions.php
+++ functions.php
@@ -270,3 +270,17 @@ function activar_lightbox_thickbox() {
     <?php
 }
 add_action('wp_footer', 'activar_lightbox_thickbox');
+remove_action('wp_head', 'wp_generator');
+add_filter('the_generator', '__return_empty_string');
+add_filter('xmlrpc_enabled', '__return_false');
+add_filter( 'rest_endpoints', function( $endpoints ) {
+    if ( isset( $endpoints['/wp/v2/users'] ) ) {
+        unset( $endpoints['/wp/v2/users'] );
+    }
+    if ( isset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] ) ) {
+        unset( $endpoints['/wp/v2/users/(?P<id>[\d]+)'] );
+    }
+    return $endpoints;
+});
+add_filter( 'wp_sitemaps_users_enabled', '__return_false' );
+
